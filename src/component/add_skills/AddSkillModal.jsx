d3e218fab5404553f3f5f4da9b75e81634add5e0
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-10-02 21:45:47 -0700

    Update design for skills adding also simple adding flow

--- src/component/add_skills/AddSkillModal.jsx
+++ src/component/add_skills/AddSkillModal.jsx
@@ -1,40 +1,109 @@
 import { useState } from "react";
 import "./AddSkillModal.css"
+import AddedSkillCards from "./AddedSkillsCard";
+
+let added_skills = [
+    {   
+        "name": "JavaScript",
+        "proficiency": "2-4 years",
+        "logo": "example-logo.png"   
+    },
+    {   
+        "name": "React",
+        "proficiency": "1-3 years",
+        "logo": "example-logo.png"   
+    }
+]
 
 function AddSkillModal({onClose}){
-    const [numberOfSkills, setNumberOfSkills] = useState(0);
+    const [numberOfSkills, setNumberOfSkills] = useState(added_skills.length);
+    const [skillName, setSkillName] = useState("");
+    const [skillProficiency, setSkillProficiency] = useState("");
+    const [skillLogo, setSkillLogo] = useState("");
+
+    //This addSkill function will be updated for Api POst request
+    const addSkill = () => {
+        added_skills.push({
+            "name": skillName,
+            "proficiency": skillProficiency,
+            "logo": skillLogo
+        })
+
+        setNumberOfSkills(added_skills.length);
+    }
+
+    const handleSkillNameChange = (event) => {
+        setSkillName(event.target.value);
+    }
+
+    const handleSkillProficiencyChange = (event) => {
+        setSkillProficiency(event.target.value);
+    }
+    const handleSkillLogoChange = (event) => {
+        setSkillLogo(event.target.value);
+    }
 
     return (
         <div className="modal-background" onClick={onClose}>
-            <div className="modal-container">
-                <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-container"  onClick={(e) => e.stopPropagation()}>
+                <div className="modal-content">
                     <h2 className="modal-title">Add New Skill</h2>
                     <div className="skill-description">
                         <div className="description">
                             <label htmlFor="skill-detail">Skill Name:</label>
-                            <textarea name="skill-detail" id="skill-detail" placeholder="Enter skill name"></textarea>
+                            <textarea 
+                                name="skill-detail" 
+                                id="skill-detail" 
+                                placeholder="Enter skill name" 
+                                onChange={handleSkillNameChange}>
+                            </textarea>
                         </div>
                         <div className="description">
-                            <label htmlFor="proficient-detail">Proficient:</label>
-                            <textarea name="proficient-detail" id="proficient-detail" placeholder="Describe proficiency"></textarea>
+                            <label htmlFor="proficient-detail">Proficiency:</label>
+                            <textarea 
+                                name="proficient-detail" 
+                                id="proficient-detail" 
+                                placeholder="Describe proficiency" 
+                                onChange={handleSkillProficiencyChange}>
+                            </textarea>
                         </div>
                         <div className="description">
                             <label htmlFor="logo-url">Logo URL:</label>
-                            <textarea name="logo-url" id="logo-url" placeholder="Enter logo URL"></textarea>
+                            <textarea 
+                                name="logo-url" 
+                                id="logo-url" 
+                                placeholder="Enter logo URL" 
+                                onChange={handleSkillLogoChange}>
+                            </textarea>
                         </div>
                     </div>
                     <div className="modal-buttons">
-                        <button className="add-skill-btn">Add More Skills</button>
-                        <button className="submit-btn" onClick={onClose}>Submit</button>
+                        <button className="add-skill-btn" onClick={addSkill}>Add More Skills</button>
                     </div>
-                    
+                </div>
+
+                <div className="added-skill-content">
                     {/* Added skills */}
+                    <div className="added-skills-section">
+                        {
+                            added_skills ? (
+                                added_skills.map((skill, index) => (
+                                    <div key={index}>
+                                      <AddedSkillCards skill={skill}/> 
+                                    </div>
+                                ))
+                            ) : (
+                                "empty"
+                            )
+                        }
+                    </div>
                     <div>
                         <p>Added skills {numberOfSkills}</p>
                     </div>
-                    <div className="added-skills-section">
-
+                    <div className="modal-buttons">
+                        <button className="submit-btn" onClick={onClose}>Submit</button>
                     </div>
+
                 </div>
             </div>
         </div>
