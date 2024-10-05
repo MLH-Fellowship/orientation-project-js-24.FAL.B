7a73730159f4db800fd0a5b99ef892ae97bd82b7
C. <cairohyfr@gmail.com>
2024-10-04 20:46:28 -0500

    fix: fix app.test (nhan's solution)

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+test("renders Resume Builder heading", () => {
+  render(<App />);
+  const headingElement = screen.getByText(/resume builder/i);
+  expect(headingElement).toBeInTheDocument();
+});
