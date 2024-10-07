9af4d419cd97aaef39138fc254400ef8db5fe789
Amina <aminaopio@my.unt.edu>
2024-10-07 12:21:26 -0500

    Running prettier

--- src/drag and drop/Dropzone.jsx
+++ src/drag and drop/Dropzone.jsx
@@ -2,43 +2,41 @@ import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 const Dropzone = () => {
-    const [files, setFiles] = useState([])
-    const [rejected, setRejected] = useState([])
-    const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
-        if (acceptedFiles?.length) {
-            setFiles(previousFiles => [
-            ...previousFiles,
-            ...acceptedFiles.map(file =>
-                Object.assign(file, { preview: URL.createObjectURL(file) })
-            )
-            ])
-        }
-    
-        if (rejectedFiles?.length) {
-            setRejected(previousFiles => [...previousFiles, ...rejectedFiles])
-        }
-      }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const [files, setFiles] = useState([]);
+  const [rejected, setRejected] = useState([]);
+  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+    if (acceptedFiles?.length) {
+      setFiles((previousFiles) => [
+        ...previousFiles,
+        ...acceptedFiles.map((file) =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        ),
+      ]);
+    }
 
-    return (
+    if (rejectedFiles?.length) {
+      setRejected((previousFiles) => [...previousFiles, ...rejectedFiles]);
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
     <form>
-        <div {...getRootProps()}>
+      <div {...getRootProps()}>
         <input {...getInputProps()} />
         {isDragActive ? (
-            <p>Drop the files here ...</p>
+          <p>Drop the files here ...</p>
         ) : (
-            <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag 'n' drop some files here, or click to select files</p>
         )}
-        </div>
-        {/* Preview */}
-        <ul>
-            {files.map(file =>(
-                <li key = {file.name}>
-                    {file.name}
-                </li>
-            ))}
-        </ul>
+      </div>
+      {/* Preview */}
+      <ul>
+        {files.map((file) => (
+          <li key={file.name}>{file.name}</li>
+        ))}
+      </ul>
     </form>
-    );
+  );
 };
 export default Dropzone;
